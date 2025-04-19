7b92170cd7749f4ec2bc595cc1e27efa17501a16
STDquantum <405720329@qq.com>
2025-04-19 21:11:28 +0800

    不压缩源码

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,9 @@ export default defineConfig({
     'import.meta.env.VERCEL': JSON.stringify(process.env.VERCEL),
   },
   build: {
-    manifest: true,
+    minify: false, // 禁用代码压缩（默认是 'esbuild'）
+    sourcemap: true, // 生成 sourcemap 文件（可选）
+    manifest: false,
     outDir: './dist', // for user easy to use, vercel use default dir -> dist
     rollupOptions: {
       output: {
